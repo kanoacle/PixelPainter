5672ab2532eb4936396a85d7998eb71a781facdb
Andrew Tram <MyMBP@Andrews-MacBook-Pro.local>
2017-03-24 19:48:48 -1000

    stash

---
+++ js/PixelPainter.js
@@ -0,0 +1,20 @@
+var bigDiv = document.getElementById('pixelPainter');
+
+
+// div for the color box
+document.createElement('div');
+
+
+
+
+//div for the button box
+document.createElement('div');
+
+
+
+
+//div for the grid box
+document.createElement('div');
+
+
+
