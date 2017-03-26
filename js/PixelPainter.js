b1ab664ccd1d5095f99d8fb38ec8e4f6c839e973
Kanojah <kanoa.hi@gmail.com>
2017-03-25 17:00:46 -1000

    clickin

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -8,16 +8,14 @@ var btns = document.createElement('DIV');
 var table = document.createElement('TABLE');
 var gridNum = 100;
 var selected;
-var columnsColor = 6;
-var colorNum = 5;
+var colorNum = 30;
 var columnsGrid = 25;
 var pixelNum = 25;
 var pixels = document.querySelectorAll('.pixels');
 var btnSel;
 
 //color selection arrays
-var colorSelection = [];
-var colorRed = ['#870000', '#af0000', '#dd0000', '#ff0000', '#ff3d3d'];
+var colorSelection = ['#000000', '#424242', '#666666', '#919191', '#c1c1c1', '#e0e0e0', '#bc0000', '#bc6a00', '#bc9c00', '#10a000', '#0007a0', '#7200a0', '#e00000', '#e08e00', '#ffd800', '#00bf16', '#1600e2', '#a100e2', '#ff2323', '#ffaa22', '#ffdc30', '#3bff14', '#1160ff', '#cb11ff', '#ff2b2b', '#ffc526', '#fffa00', '#75ff85', '#4c9fff', '#da59ff'];
 
 //codes for border box
 border.onload = (function() {
@@ -30,32 +28,24 @@ border.onload = (function() {
 })();
 
 //codes for color boxes
-for (var c = 0; c < columnsColor; c++) {
-  var column = document.createElement('DIV');
-    column.style.width = '16.65%';
-    column.style.minHeight = '100%';
-    column.style.border = '1px solid #333';
-    column.style.display = 'inline-block';
-    var i = 0;
-    while (i < colorNum) {
-      var color = document.createElement('DIV');
-      color.id = `color ${i}`;
-      color.className = 'pixels';
-      color.style.width = '100%';
-      color.style.height = '20%';
-      color.style.backgroundColor = colorRed[i];
-      color.style.border = '1px solid #333';
-      color.style.display = 'table-col';
-      color.onclick = (function( event ) {
-          btnSel = event.srcElement;
-          selected = btnSel.style.backgroundColor;
-          console.log(btnSel);
-      });
-      column.appendChild(color);
-      i++;
-    }
-  colors.appendChild(column);
+for (var c = 0; c < colorNum; c++) {
+  var color = document.createElement('DIV');
+  color.id = `color ${c}`;
+  color.style.width = '14.65%';
+  color.style.minHeight = '18%';
+  color.style.margin = '1%';
+  color.style.border = '3px solid #333';
+  color.style.borderRadius = '25px';
+  color.style.display = 'inline-block';
+  color.style.backgroundColor = colorSelection[c];
+  color.onclick = (function() {
+    btnSel = event.srcElement;
+    selected = btnSel.style.backgroundColor;
+  });
+  colors.appendChild(color);
 }
+
+
 
 //codes for pixel boxes
 for (var c = 0; c < columnsGrid; c++) {
@@ -88,7 +78,6 @@ for (var c = 0; c < columnsGrid; c++) {
 border.appendChild(colors);
 
 colors.onload = (function() {
-  colors.style.border = '3px solid #333';
   colors.style.width = '35%';
   colors.style.height = '43%';
   colors.style.float = 'left';
